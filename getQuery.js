ac30d7e8fff00f1f7aa36334c65bb6991f6a9815
PapiKKo <65007528+PapiKKo@users.noreply.github.com>
2021-10-07 13:36:27 +0900

    10.7_upload

---
+++ getQuery.js
@@ -0,0 +1,33 @@
+function execute() {
+    var yoga = document.sampleForm.menu[0];
+    var study = document.sampleForm.menu[1];
+    var meal = document.sampleForm.menu[2];
+    var result = document.getElementById("result");
+    result.innerHTML = "";
+    var none = true;
+    const textbox = document.getElementById("input-message");
+    const inputValue = textbox.value;
+    const url = new URL(inputValue);
+    const params = new URLSearchParams(url.search);
+ 
+    if(yoga.checked) {
+        result.innerHTML = yoga.value + "が選択されています。";
+	window.open("http://localhost:4245/srt.js/index.htm?"+params+"&surl=sample/example2_srt.js",'_blank');
+        none = false;
+    }
+ 
+    if(study.checked) {
+        result.innerHTML += study.value + "が選択されています。";
+        none = false;
+    }
+ 
+    if(meal.checked) {
+        result.innerHTML += meal.value + "が選択されています。";
+        none = false;
+    }
+ 
+    if(none) {
+        result.innerHTML = "何も選択されていません。";
+    }
+ 
+}
